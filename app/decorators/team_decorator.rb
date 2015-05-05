786a4f682777b76041817c9d66e663ded8f75af6
DJ Greenfield <mr.kaitryn.dale@gmail.com>
2015-05-04 19:29:58 -0600

    Converted team total stat decorator into an openstruct.

--- app/decorators/team_decorator.rb
+++ app/decorators/team_decorator.rb
@@ -4,9 +4,7 @@ class TeamDecorator < Draper::Decorator
 
   def team_total
     pokemon_attributes = [:hit_points, :attack, :defense, :special_attack, :special_defense, :speed]
-    pokemon_attributes.each_with_object(Hash.new(0)) do |attr, hash|
-      hash[attr] = total(attr)
-    end
+    OpenStruct.new(pokemon_attributes.each_with_object(Hash.new(0)) { |attr, hash| hash[attr] = total(attr) })
   end
 
   def total(attribute)
