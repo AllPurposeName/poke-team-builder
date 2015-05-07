35ffad755024f1131bea8381d8fce946f0c353f9
DJ Greenfield <mr.kaitryn.dale@gmail.com>
2015-05-07 11:10:05 -0600

    heroku just learn angular already

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,7 @@ Bundler.require(*Rails.groups)
 module Reader
   class Application < Rails::Application
     config.assets.precompile << /\.(?:svg|eot|woff|ttf)\z/
+    config.assets.js_compressor = Uglifier.new(mangle: false)
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
